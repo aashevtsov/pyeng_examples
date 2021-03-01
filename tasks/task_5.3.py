122c2da35efdf9d527b4259a246aaade0375855a
aashevtsov <work.shevcov@gmail.com>
2021-03-01 21:17:43 +0300

    Tasks folder

---
+++ tasks/task_5.3.py
@@ -0,0 +1,22 @@
+access_template = [
+    "switchport mode access",
+    "switchport access vlan {}",
+    "switchport nonegotiate",
+    "spanning-tree portfast",
+    "spanning-tree bpduguard enable",
+]
+
+trunk_template = [
+    "switchport trunk encapsulation dot1q",
+    "switchport mode trunk",
+    "switchport trunk allowed vlan {}",
+]
+template = {'access':access_template,'trunk':trunk_template}
+question = {'access': "Введите норме влана: ",'trunk': "введите разрешенные вланы: "}
+
+mode = input("Ввежите режим работы интерфейса: ")
+interface = input("Введите тип и номер интерфейса: ")
+vlans = input("Введите норме влана(ов): ")
+
+print(f"Interface,{interface}",'\n')
+print('\n'.join(template[mode]).format(vlans))
